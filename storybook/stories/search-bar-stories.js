90d93aab9443662becfc9179971c0d124b0a7b81
topwebdev0612 <81346046+topwebdev0612@users.noreply.github.com>
2021-03-30 02:36:01 -0700

    Revert "initial commit"
    This reverts commit 629ab6f6bc5a5343be672477298ef8579a376c7d.

--- storybook/stories/search-bar-stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {storiesOf} from '@storybook/react-native';
-import {Container, SearchBar} from '@src/components/elements';
-
-storiesOf('SearchBar', module).add('SearchBar', () => (
-  <Container style={{padding: 10}}>
-    <SearchBar placeholder="Find places, dishes, restaurants..." />
-  </Container>
-));
